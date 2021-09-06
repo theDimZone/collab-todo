c38ccb6b77a2f346e874500a8ae42781277b6386
theDimZone <thedimzone99@gmail.com>
2021-09-06 17:17:24 +0300

    remove ecto

--- mix.exs
+++ mix.exs
@@ -34,8 +34,6 @@ defmodule CollabTodo.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.5.12"},
-      {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
